dd59bb77c4996f6fcba98aaedde784d2a2cd8701
Paula Ponce <paula.poncez@usach.cl>
2017-06-05 20:46:58 -0400

    esquema usando la propiedad position con DOM

---
+++ js/main.js
@@ -0,0 +1,66 @@
+var cancha = document.getElementById("cancha");
+cancha.style.width = "1000px";
+cancha.style.height = "600px";
+cancha.style.background = "#207728";
+cancha.style.position = "relative";
+
+var area = document.getElementById("area");
+area.style.width = "960px";
+area.style.height = "560px";
+area.style.background = "transparent";
+area.style.border = "2px solid white";
+area.style.position = "absolute";
+area.style.top = "20px";
+area.style.left = "20px";
+
+var arcoIzq = document.getElementById("arco-izq");
+arcoIzq.style.width = "200px";
+arcoIzq.style.height = "280px";
+arcoIzq.style.background = "transparent";
+arcoIzq.style.border = "2px solid white";
+arcoIzq.style.position = "absolute";
+arcoIzq.style.top = "140px";
+arcoIzq.style.left = "-2px";
+
+var centro = document.getElementById("centro");
+centro.style.width = "100px";
+centro.style.height = "100px";
+centro.style.background = "transparent";
+centro.style.border = "2px solid white";
+centro.style.borderRadius = "50%";
+centro.style.position = "absolute";
+centro.style.top = "230px";
+centro.style.left = "430px";
+
+var arcoDer = document.getElementById("arco-der");
+arcoDer.style.width = "200px";
+arcoDer.style.height = "280px";
+arcoDer.style.background = "transparent";
+arcoDer.style.border = "2px solid white";
+arcoDer.style.position = "absolute";
+arcoDer.style.top = "140px";
+arcoDer.style.right = "-2px";
+
+var bola1 = document.getElementById("bola-1");
+bola1.style.width = "24px";
+bola1.style.height = "24px";
+bola1.style.backgroundImage = "url('img/bola.png')";
+bola1.style.position = "absolute";
+bola1.style.top = "140px";
+bola1.style.left = "190px";
+
+var bola2 = document.getElementById("bola-2");
+bola2.style.width = "24px";
+bola2.style.height = "24px";
+bola2.style.backgroundImage = "url('img/bola.png')";
+bola2.style.position = "absolute";
+bola2.style.top = "80px";
+bola2.style.left = "420px";
+
+var bola3 = document.getElementById("bola-3");
+bola3.style.width = "24px";
+bola3.style.height = "24px";
+bola3.style.backgroundImage = "url('img/bola.png')";
+bola3.style.position = "absolute";
+bola3.style.top = "400px";
+bola3.style.right = "200px";
